a7c378fc2ba072ba167f8c7505fabca7b3e960e5
Muhammad Usman <70320667+MuhammadUsmanTipu@users.noreply.github.com>
2023-02-09 20:27:47 +0500

    Add files via upload

---
+++ Construction of Matrix and Indexing.R
@@ -0,0 +1,10 @@
+I am constructing a matrix and it has a range of values from 10 to 29. It has 5 columns and 4 rows. Then we do indexing and print first and second row.
+ 
+ 
+ # Constructing a matrix of 4 rows and 5 columns
+ usman<-matrix(10:29,nrow=4,ncol=5)
+ #checking the values in a matrix form
+ usman
+ #indexing is being done from row 1 to row 2.
+ usman [1:2,]
+ 
