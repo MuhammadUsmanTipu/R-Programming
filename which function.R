32e51966056a9b7ef2f903510ca72ac56ffd5d1b
Muhammad Usman <70320667+MuhammadUsmanTipu@users.noreply.github.com>
2023-02-12 00:46:12 +0500

    Add files via upload

---
+++ which function.R
@@ -0,0 +1,8 @@
+#Constructing a matrix and then indices of a specific number within that matrix is find by using which 
+ 
+ #Constructing matrix
+ usman<-matrix(10:29,nrow=4,ncol=5)
+ #indicing for a specific number 19.
+ which (usman==19)
+ 
+ 
