a396428a0736925768f8c97ecbce166016c2f75d
Muhammad Usman <70320667+MuhammadUsmanTipu@users.noreply.github.com>
2023-02-12 21:39:30 +0500

    Add files via upload

---
+++ indexing through one statement.R
@@ -0,0 +1,7 @@
+#I construct a list with different data types i.e. character and numeric containing values. Then I do the indexong for the second element of numeric data type i.e.usman. [[]] is used to do indexing within a list.It will check usman and then gave us the second element of numeric vector in x and x is for printing  
+ 
+ #Indexing within a list of different datatypes
+ x<-list("datascience", usman=c(10,9,8,1,2,3)) [["usman"]] [2]
+ #printing second element of numeric vector usman
+ x
+ 
